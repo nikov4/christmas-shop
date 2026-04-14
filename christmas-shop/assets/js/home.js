86303d64b6d1f2e9037724d6a0120b4c77d810ee
Nikolai O. <nik@o84.ru>
2026-04-14 22:04:17 +0300

    feat: add timer for CTA section

---
+++ christmas-shop/assets/js/home.js
@@ -0,0 +1,60 @@
+/*
+Implementation of the Timer on the home page: +8
+    + The timer shows the correct value of the remaining days, hours, minutes, and seconds until the New Year in UTC+0: +2
+    + Leading zeros are not displayed for single-digit numbers: +2
+    ++ The timer updates every second, displaying the current remaining time with the labels days, hours, minutes, seconds (the labels must not change): +4
+*/
+
+let timerDays = 0;
+let timerHours = 0;
+let timerMinutes = 0;
+let timerSeconds = 0;
+let daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+// Timer
+function Timer() {
+  // get current
+  const date = new Date();
+  const currentYear = date.getFullYear();
+  const currentMonth = date.getMonth();
+  const currentDay = date.getDate();
+  //const currentHour = date.getHours();
+  const currentHour = date.getUTCHours();
+  const currentMinute = date.getMinutes();
+  const currentSeconds = date.getSeconds();
+
+  /* console.log("currentYear=",currentYear,"currentMonth=",currentMonth,"currentDay=",currentDay,"currentHour=",currentHour,"currentMinute=",currentMinute,"currentSeconds=",currentSeconds); */
+
+  //check for leap
+  const leapYearDate = new Date(currentYear, 1, 29);
+  let leapYear = leapYearDate.getMonth();
+  if (leapYear === 1) {
+    daysInMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  }
+
+  // calc timer
+  timerDays = daysInMonth[currentMonth] - currentDay;
+  for (let i = currentMonth + 1; i <= 11; i += 1) {
+    timerDays = timerDays + daysInMonth[i];
+  }
+  timerHours = 23 - currentHour;
+  timerMinutes = 59 - currentMinute;
+  timerSeconds = 59 - currentSeconds;
+
+  // set values
+  document.querySelector(
+    ".timer-items-container .timer-item .timer-days",
+  ).textContent = timerDays;
+  document.querySelector(
+    ".timer-items-container .timer-item .timer-hours",
+  ).textContent = timerHours;
+  document.querySelector(
+    ".timer-items-container .timer-item .timer-minutes",
+  ).textContent = timerMinutes;
+  document.querySelector(
+    ".timer-items-container .timer-item .timer-seconds",
+  ).textContent = timerSeconds;
+}
+
+// Run timer
+const timerId = setInterval(Timer, 1000);
